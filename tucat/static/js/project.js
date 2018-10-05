5194b0d3328b9136bdc03d27ce30854d82289901
natoinet <antoine.brunel@gmail.com>
2018-10-05 16:34:34 +0200

    Fixing export download button

--- tucat/static/js/project.js
+++ tucat/static/js/project.js
@@ -1,1 +1,12 @@
 /* Project specific Javascript goes here. */
+if (!$) {
+    $ = django.jQuery;
+}
+
+download_file = function(pk) {
+  $('input[name="_selected_action"]').removeAttr('checked');
+  $('input[name="_selected_action"][value="{}"]'.replace('{}', pk)).attr('checked', 'checked');
+  $('select[name="action"]').val('download');
+  $('button[type="submit"][name="index"]').click();
+  $('input[name="_selected_action"]').removeAttr('checked');
+};
