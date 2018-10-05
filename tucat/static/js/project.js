bd90fd16badf6c7331983290a43c425d918f73dd
natoinet <antoine.brunel@gmail.com>
2018-10-05 17:14:54 +0200

    Fixing export download button bug

--- tucat/static/js/project.js
+++ tucat/static/js/project.js
@@ -8,5 +8,4 @@ download_file = function(pk) {
   $('input[name="_selected_action"][value="{}"]'.replace('{}', pk)).attr('checked', 'checked');
   $('select[name="action"]').val('download');
   $('button[type="submit"][name="index"]').click();
-  $('input[name="_selected_action"]').removeAttr('checked');
 };
