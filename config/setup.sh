0b426f6d41037c2e79485aa1f8f4cc7b5bfbbd6f
natoinet <antoine.brunel@gmail.com>
2018-10-06 17:24:39 +0200

    Resolving migrate error : relation users_user does not exist

--- config/setup.sh
+++ config/setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+sudo docker-compose run --rm djangoapp python manage.py makemigrations admin_interface auth sessions sites django_celery_beat core users
 sudo docker-compose run --rm djangoapp python manage.py migrate
 sudo docker-compose run --rm djangoapp python manage.py createsuperuser
 sudo docker-compose run --rm djangoapp chmod +x ./config/fixtures/load.sh
