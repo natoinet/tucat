9d15491989e2501739fa7a55a523ebf784d67398
natoinet <antoine.brunel@gmail.com>
2018-10-11 00:28:50 +0200

    Fixing users_user migration problem with fake-initial command

--- config/setup.sh
+++ config/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo docker-compose run --rm djangoapp python manage.py makemigrations admin_interface auth sessions sites django_celery_beat core users
-sudo docker-compose run --rm djangoapp python manage.py migrate
+sudo docker-compose run --rm djangoapp python manage.py migrate --fake-initial
 sudo docker-compose run --rm djangoapp python manage.py createsuperuser
 sudo docker-compose run --rm djangoapp chmod +x ./config/fixtures/load.sh
 sudo docker-compose run --rm djangoapp ./config/fixtures/load.sh
