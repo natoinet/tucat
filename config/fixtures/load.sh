bc4efe9ee1e1ef177db33b61df827c38c8eb9e36
natoinet <antoine.brunel@gmail.com>
2018-10-14 01:22:26 +0200

    Improving fixture loading

--- config/fixtures/load.sh
+++ config/fixtures/load.sh
@@ -4,17 +4,26 @@ python manage.py loaddata admin_interface_theme_tucat.json
 
 python manage.py loaddata auth.json
 
-if [ -e config/fixtures/account.json ]
-then
-  python manage.py loaddata account.json
+fixture=account
+if [ -e config/fixtures/$fixture.json ] then
+  echo 'Importing $fixture'
+  python manage.py loaddata $fixture.json
+else
+  echo 'No $fixture to import'
 fi
 
-if [ -e config/fixtures/users.json ]
-then
-  python manage.py loaddata users.json
+fixture=users
+if [ -e config/fixtures/$fixture.json ] then
+  echo 'Importing $fixture'
+  python manage.py loaddata $fixture.json
+else
+  echo 'No $fixture to import'
 fi
 
-if [ -e config/fixtures/socialaccount.json ]
-then
-  python manage.py loaddata socialaccount.json
+fixture=socialaccount
+if [ -e config/fixtures/$fixture.json ] then
+  echo 'Importing $fixture'
+  python manage.py loaddata $fixture.json
+else
+  echo 'No $fixture to import'
 fi
