3741bdb28cb7eee45f6129bbc8acbd9c54fb229f
natoinet <antoine.brunel@gmail.com>
2018-10-21 16:25:41 +0200

    Splitting Tucat from  Plugin fixtures

--- config/fixtures/dump.sh
+++ config/fixtures/dump.sh
@@ -6,6 +6,3 @@ python manage.py dumpdata sites --indent 4 -o ~/sites.json
 python manage.py dumpdata account --indent 4 -o ~/account.json
 python manage.py dumpdata socialaccount --indent 4 -o ~/socialaccount.json
 python manage.py dumpdata users --indent 4 -o ~/users.json
-python manage.py dumpdata twitter_extraction.exportationtype --indent 4 -o ~/twitter_extraction.exportationtype.json
-python manage.py dumpdata twitter_extraction.exportationformat --indent 4 -o ~/twitter_extraction.exportationformat.json
-python manage.py dumpdata twitter_extraction.twitterlistextraction --indent 4 -o ~/twitter_extraction.twitterlistextraction.json
